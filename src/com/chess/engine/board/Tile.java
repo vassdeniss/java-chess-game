1d352b5ba731cbe6ed6975765d54795ab183bd92
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-03-18 11:38:59 +0200

    Made classes immutable
    Made classes immutable
    Ordered classes in packages
    Added Glava dependency for immutable maps

---
+++ src/com/chess/engine/board/Tile.java
@@ -0,0 +1,89 @@
+package com.chess.engine.board;
+
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+    // Int for tile location
+    // int tileCoordinate - mutable - not good for a lot of reasons
+    // i.e (garbage collection, memory etc...)
+    protected final int tileCoordinate;
+    // Protected i.e only subclases can access it
+    // final - once set it cannot be overwritten
+
+    // Dictionary basically
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllEmptyTiles();
+
+    // Method to set empty tiles on grid
+    private static Map<Integer, EmptyTile> createAllEmptyTiles() {
+        // Create a hashmap
+        final Map<Integer, EmptyTile> tileMap = new HashMap<>();
+
+        // Loop 64 times (chess grid = 8x8) and put each number in the dictionary
+        for (int i = 0; i < 64; i++) {
+            tileMap.put(i, new EmptyTile(i));
+        }
+
+        // Return immutable map from the Guava library
+        return ImmutableMap.copyOf(tileMap);
+    }
+
+    // The only constructor than will be used
+    public static Tile createTile(final int coordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(coordinate, piece) : EMPTY_TILES.get(coordinate);
+    }
+
+    // Constructor for setting location
+    private Tile(final int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    // True or false depending if piece is on tile
+    // Get the piece if its occupied
+    public abstract boolean isTileOccupied();
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+        EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+
+        // Return false - no piece on empty tile
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        // Return null - no piece on empty tile
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+        // Type of Piece for recognizing the piece
+        // private - only for this subclass
+        private final Piece tilePiece;
+
+        OccupiedTile(int coordinate, Piece tilePiece) {
+            super(coordinate);
+            this.tilePiece = tilePiece;
+        }
+
+        // Return true - there will be always a piece on the tile
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        // Return tilePiece - there will be always a piece on the tile
+        @Override
+        public Piece getPiece() {
+            return this.tilePiece;
+        }
+    }
+}
