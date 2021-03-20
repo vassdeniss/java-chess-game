d98f0a165b5560c100d4bd07b2f00db9d35de5b2
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-03-20 19:22:21 +0200

    Even More Work on the Pawn Class
    Written more code to the Pawn Class
    Added comments on the Pawn Class
    Code Formatting

--- src/com/chess/engine/Alliance.java
+++ src/com/chess/engine/Alliance.java
@@ -3,35 +3,23 @@ package com.chess.engine;
 public enum Alliance {
     WHITE {
         @Override
-        public int getDirection() {
-            return -1;
-        }
+        public int getDirection() { return -1; }
 
         @Override
-        public boolean isWhite() {
-            return true;
-        }
+        public boolean isWhite() { return true; }
 
         @Override
-        public boolean isBlack() {
-            return false;
-        }
+        public boolean isBlack() { return false; }
     },
     BLACK {
         @Override
-        public int getDirection() {
-            return 1;
-        }
+        public int getDirection() { return 1; }
 
         @Override
-        public boolean isWhite() {
-            return false;
-        }
+        public boolean isWhite() { return false; }
 
         @Override
-        public boolean isBlack() {
-            return true;
-        }
+        public boolean isBlack() { return true; }
     };
 
     public abstract int getDirection();
--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -11,9 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class Pawn extends Piece {
-    // No comments on this class as it is not finished
-
-    private final static int[] POSSIBLE_MOVE_COORDINATES = {8, 16};
+    private final static int[] POSSIBLE_MOVE_COORDINATES = {7, 8, 9, 16};
 
     Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -23,27 +21,70 @@ public class Pawn extends Piece {
     public Collection<Move> legalMoves(final Board board) {
         final List<Move> legalMoves = new ArrayList<>();
 
+        // Loop through all possible moves
+        // Note*: 8 is normal move, 16 is jump, 7 and 9 are diagonal attack moves
         for (final int currentPossibleMove : POSSIBLE_MOVE_COORDINATES) {
             final int possibleDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * currentPossibleMove);
 
+            // if the tile is valid continue
             if (BoardUtils.isValidTileCoordinate(possibleDestinationCoordinate)) {
                 continue;
             }
 
+            // if the move is 8 (normal) and the tile is not occupied
+            // add a normal move
+            // STUB
             if (currentPossibleMove == 8 && !board.getTile(possibleDestinationCoordinate).isTileOccupied()) {
                 legalMoves.add(/*placeholder*/);
-            } else if (
-                    currentPossibleMove == 16
-                    && this.isFirstMove()
-                    && (BoardUtils.SECOND_ROW(this.piecePosition)
-                        && this.pieceAlliance.isBlack())
-                    || (BoardUtils.SEVENTH_ROW(this.piecePosition)
-                        && this.pieceAlliance.isWhite())
-                    ) {
-                final int behindPossibleDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+            // Determine the move is 16 (jump) (only executable on first move)
+            // Then determine wheres the pawn i.e if its black and on second row or
+            // white on seventh row
+            } else if (currentPossibleMove == 16 && this.isFirstMove() &&
+                    (BoardUtils.SECOND_ROW(this.piecePosition) && this.pieceAlliance.isBlack())
+                    || (BoardUtils.SEVENTH_ROW(this.piecePosition) && this.pieceAlliance.isWhite())) {
+                // get for the tile behind the jumped tile
+                final int behindPossibleDestinationCoordinate = this.piecePosition +
+                        (this.pieceAlliance.getDirection() * 8
+                );
+
+                // if the behind tile is not occupied and and the jump tile is not occupied
+                // add a jump move
+                // STUB
                 if (!board.getTile(behindPossibleDestinationCoordinate).isTileOccupied
                     && !board.getTile(possibleDestinationCoordinate).isTileOccupied) {
                     legalMoves.add(/*placeholder*/);
+                }
+            // If the attack is 7 (diagonal attack right)
+            // and if the piece is white and not on the eight column (impossible diagonal attack)
+            // and if the piece is black and not on the first column (impossible diagonal attack)
+            // continue
+            } else if (currentPossibleMove == 7 &&
+                    !((BoardUtils.EIGHT_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite()) ||
+                    (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()))) {
+                // if the tile is occupied add an attack move
+                // STUB
+                if (board.getTile(possibleDestinationCoordinate).isTileOccupied) {
+                    final Piece pieceAtTile = board.getTile(possibleDestinationCoordinate).getPice();
+
+                    if (this.pieceAlliance != pieceAtTile.getPieceAlliance()) {
+                        legalMoves.add(/*placeholder*/);
+                    }
+                }
+            // If the attack is 9 (diagonal attack left)
+            // and if the piece is white and not on the first column (impossible diagonal attack)
+            // and if the piece is black and not on the eight column (impossible diagonal attack)
+            // continue
+            } else if (currentPossibleMove == 9 &&
+                    !((BoardUtils.EIGHT_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()) ||
+                    (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite()))) {
+                // if the tile is occupied add an attack move
+                // STUB
+                if (board.getTile(possibleDestinationCoordinate).isTileOccupied) {
+                    final Piece pieceAtTile = board.getTile(possibleDestinationCoordinate).getPice();
+
+                    if (this.pieceAlliance != pieceAtTile.getPieceAlliance()) {
+                        legalMoves.add(/*placeholder*/);
+                    }
                 }
             }
         }
